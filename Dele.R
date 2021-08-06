14661c24eb43525664244d78a35b04e1ae52612a
Peltunde <74152053+Peltunde@users.noreply.github.com>
2021-08-06 19:04:51 +0800

    First Task

---
+++ Dele.R
@@ -0,0 +1,11 @@
+name<- c("Ola") 
+email<- c("oladele.olatunde@yahoo.com")
+slack_username<- c("@Dele")
+Twitter_handle<-c("@deletunde_")
+Hamming_distance<-c(sum(Twitter_handle != slack_username))
+
+cat("name:", name,
+    "email:", email,
+    "slack username:", slack_username,  
+    "Twitter Handle:", Twitter_handle, 
+    "Hamming Distance:", Hamming_distance)
